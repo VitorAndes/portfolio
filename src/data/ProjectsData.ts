f050a9d693f913d9a2a7b2a90e2810c44b658b49
Vitor andes <vitor.andes.santos04@gmail.com>
2025-10-20 16:30:53 -0300

    removed 2 projects

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -24,25 +24,7 @@ export const ProjectsData: ProjectsType[] = [
     image: "https://i.imgur.com/0APFotZ.png",
     link: "https://haru-hub-frontend.vercel.app/",
     techs: ["react", "typescript", "tailwind"],
-    github: "https://github.com/VitorAndes/Haru-Hub_frontend",
-  },
-  {
-    title: "Powerblessed",
-    description:
-      "Landing page moderna desenvolvida para uma empresa de energia solar, focada em apresentar serviços, transmitir credibilidade e facilitar o contato com clientes, com design limpo e responsivo.",
-    image: "https://i.imgur.com/ZWsBzXr.png",
-    link: "https://powerblessed.vercel.app/",
-    techs: ["vue", "less", "typescript"],
-    github: "https://github.com/VitorAndes/powerblessed",
-  },
-
-  {
-    title: "EsteticaGio",
-    description:
-      "Landing page elegante para serviços de estética, com layout profissional, paleta de cores harmoniosa e foco em transmitir confiança e atrair clientes de forma eficiente.",
-    image: "https://i.imgur.com/RfRY1k7.png",
-    link: "https://esteticagio.vercel.app/",
-    techs: ["vue", "typescript", "less"],
-    github: "https://github.com/VitorAndes/Esteticagio",
+    github:
+      "https://github.com/VitorAndes/Haru-Hub_frontend",
   },
 ];
